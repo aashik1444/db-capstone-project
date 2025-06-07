9e560aad81bfe96a736ea45abe3ee9359cf89496
aashik1444 <aashiksandeep4@gmail.com>
2025-06-07 14:49:40 +0530

    Add Little Lemon database SQL file and project structure

---
+++ menus-populating.sql
@@ -0,0 +1,14 @@
+INSERT INTO Menus (MenuID, ItemID, Cuisine)
+VALUES
+(1, 1, 'Greek'),
+(1, 7, 'Greek'),
+(1, 10, 'Greek'),
+(1, 13, 'Greek'),
+(2, 3, 'Italian'),
+(2, 9, 'Italian'),
+(2, 12, 'Italian'),
+(2, 15, 'Italian'),
+(3, 5, 'Turkish'),
+(3, 17, 'Turkish'),
+(3, 11, 'Turkish'),
+(3, 16, 'Turkish');
